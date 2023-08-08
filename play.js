b89f9abdbeba16c5c9a8aa322a4ced096a50d327
JuliaMaxy <jjuliamaxxx@gmail.com>
2023-08-08 16:12:18 +0300

    feat: get user input
    Use while loop to continue asking user to make the move until
    they input a valid one

--- play.js
+++ play.js
@@ -1,7 +1,20 @@
-const options = ['Rock', 'Paper', 'Scissors'];
+const options = ['rock', 'paper', 'scissors'];
+
 function getComputerChoice() {
     const randomNum = Math.floor((Math.random() * 3));
     return options[randomNum];
 }
 
-console.log(getComputerChoice())
+let playerSelection;
+while(true) {
+    playerSelection = prompt("Your move: ").toLowerCase();
+    if(options.includes(playerSelection)) {
+        break; 
+    }
+    else {
+        continue;
+    } 
+}
+
+
+console.log(getComputerChoice(), playerSelection)
