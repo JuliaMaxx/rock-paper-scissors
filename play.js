04fc64da5ff54ca8d1cce56a8b04618e663646c8
JuliaMaxy <jjuliamaxxx@gmail.com>
2023-08-08 15:45:26 +0300

    feat: create a function to chooses a random move
    Create getComputerChoice function that randomly chooses
    one of three possible moves (Rock, Paper, Scissors) and returns it

--- play.js
+++ play.js
@@ -1,1 +1,7 @@
-console.log('Hi')
+const options = ['Rock', 'Paper', 'Scissors'];
+function getComputerChoice() {
+    const randomNum = Math.floor((Math.random() * 3));
+    return options[randomNum];
+}
+
+console.log(getComputerChoice())
