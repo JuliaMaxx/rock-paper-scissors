44bc601b7b1af401f1169134ba634fb0cb02559c
JuliaMaxy <jjuliamaxxx@gmail.com>
2023-08-31 15:37:54 +0300

    chore(clean up): add comments, remove whitespace

--- play.js
+++ play.js
@@ -4,6 +4,25 @@ const options = ['Rock', 'Paper', 'Scissors'];
 // emojis that represent moves
 const emojis = {'Rock': '🤘', 'Paper':'✋', 'Scissors': '✌️'};
 
+// elements
+const main = document.getElementById('main');
+const playAgainButton = document.getElementById('play-again');
+const playAgainText = document.getElementById('play-again-text');
+const moves = document.querySelectorAll('.option');
+const result = document.getElementById('result');
+const roundExplanation = document.getElementById('explanation');
+const roundInfo = document.getElementById('round-info');
+const playerScoreDisplay = document.getElementById('player-score');
+const compScoreDisplay = document.getElementById('comp-score');
+const playerMoveEmojiDisplay = document.getElementById('player-move-emoji');
+const compMoveEmojiDisplay = document.getElementById('comp-move-emoji');
+const compAddPoint = document.querySelector('.comp.add-point');
+const playerAddPoint = document.querySelector('.player.add-point');
+const themeLight = document.querySelector('.lightBtn.theme');
+const themeDark = document.querySelector('.darkBtn.theme');
+const changeableElements = document.querySelectorAll('.change');
+
+
 // get random move from the options array
 function getComputerChoice() {
     const randomNum = Math.floor((Math.random() * 3));
@@ -15,7 +34,7 @@ function playRound(playerMove, compMove) {
 
     // moves are the same - it must be a tie
     if(playerMove === compMove){
-        return `It\'s a tie! ${playerMove} ties ${compMove}`
+        return `It\'s a tie! ${playerMove} ties ${compMove}`;
     }
     // player's move is rock
     if(playerMove === 'Rock'){
@@ -24,7 +43,7 @@ function playRound(playerMove, compMove) {
             return 'Computer wins! Rock is beaten by Paper';
         }
         else{
-            return `You win! Rock beats ${compMove}`
+            return `You win! Rock beats ${compMove}`;
         }
     }
     // player's move is paper
@@ -34,7 +53,7 @@ function playRound(playerMove, compMove) {
             return 'Computer wins! Paper is beaten by Scissors';
         }
         else{
-            return `You win! Paper beats ${compMove}`
+            return `You win! Paper beats ${compMove}`;
         }
     }
     // player's move is scissors
@@ -44,28 +63,13 @@ function playRound(playerMove, compMove) {
             return 'Computer wins! Scissors are beaten by Rock';
         }
         else{
-            return `You win! Scissors beat ${compMove}`
+            return `You win! Scissors beat ${compMove}`;
         }
     }
 }
 
-const playAgainButton = document.getElementById('play-again');
-const moves = document.querySelectorAll('.option');
-const result = document.getElementById('result');
-const playerScoreDisplay = document.getElementById('player-score');
-const compScoreDisplay = document.getElementById('comp-score');
-const roundExplanation = document.getElementById('explanation');
-const roundInfo = document.getElementById('round-info');
-const playerMoveEmojiDisplay = document.getElementById('player-move-emoji');
-const compMoveEmojiDisplay = document.getElementById('comp-move-emoji');
-const main = document.getElementById('main');
-const playAgainText = document.getElementById('play-again-text');
-const compAddPoint = document.querySelector('.comp.add-point');
-const playerAddPoint = document.querySelector('.player.add-point');
-const themeLight = document.querySelector('.lightBtn.theme');
-const themeDark = document.querySelector('.darkBtn.theme');
-const changeableElements = document.querySelectorAll('.change');
-
+// switch all the classes that start with
+// '-light' to the ones that start with '-dark'
 function changeThemes() {
     changeableElements.forEach((element) => {
         const classes = Array.from(element.classList);
@@ -85,21 +89,20 @@ function changeThemes() {
     })
 }
 
+// change themes
 function lightToDark() {
     themeLight.style.display = 'none';
     themeDark.style.display = 'block';
     changeThemes();
 }
-
-function darkToLIght() {
+function darkToLight() {
     themeLight.style.display = 'block';
     themeDark.style.display = 'none';
     changeThemes();
 }
-
-
 themeLight.addEventListener('click', lightToDark);
-themeDark.addEventListener('click', darkToLIght);
+themeDark.addEventListener('click', darkToLight);
+
 
 // initialize both scores to 0
 let playerScore = 0;
@@ -117,18 +120,21 @@ playAgainButton.addEventListener('click', () => {
     playerScoreDisplay.innerText = `You: 0`;
     compScoreDisplay.innerText = `Computer: 0`;
     playAgainButton.style.opacity = '0%';
-    main.style.opacity = '100%'
+    main.style.opacity = '100%';
 });
 
 moves.forEach((move) => {
     move.addEventListener('click', () => {
         // display the result
-        const text = playRound(move.dataset.option, getComputerChoice()).split('!')
+        const text = playRound(move.dataset.option, getComputerChoice()).split('!');
         result.innerText = text[0] + '!';
-        roundExplanation.innerText = text[1]
+        // display explanation
+        roundExplanation.innerText = text[1];
+        // make both point indicators invisible in case of a tie
         compAddPoint.style.opacity = '0';
         playerAddPoint.style.opacity = '0';
 
+        // convert moves from the text returned by PlayRound to emojis
         const roundMoves = text[1].split(' ')
         const playerMoveEmoji = emojis[roundMoves[1]];
         const compMoveEmoji = emojis[roundMoves[roundMoves.length - 1]];
@@ -164,10 +170,10 @@ moves.forEach((move) => {
             else{
                 result.innerText = 'I\'s a tie this time!';
             }
-            // disable the buttons
+            // disable the option buttons
             moves.forEach(move => move.disabled=true)
-            // ask the user to play again
             main.style.opacity = '60%';
+            // ask the user to play again
             playAgainButton.style.opacity = '100%';
         }
     });
